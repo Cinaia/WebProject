e33e374ebc52603b0e54c13c1dc814f13c313a8d
BalanceBalls <alex250mc@gmail.com>
2017-06-28 22:50:40 +0500

    1.0

---
+++ app/src/main/java/com/example/admin/cryptowatcher/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.admin.cryptowatcher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000*2);
+    }
+}
