22de74134e4d2abaab81ac5d137ad4cb7ebc3ede
BalanceBalls <alex250mc@gmail.com>
2017-06-29 19:01:06 +0500

    1.0

--- app/src/main/java/com/example/admin/cryptowatcher/MainActivity.java
+++ app/src/main/java/com/example/admin/cryptowatcher/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.admin.cryptowatcher;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,8 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
